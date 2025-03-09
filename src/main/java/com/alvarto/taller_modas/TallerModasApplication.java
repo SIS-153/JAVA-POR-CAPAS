b01123a691a6e3f534f27c0b8657b3b290c6e0e6
alvarto1296 <alvarto1296@gmail.com>
2025-03-09 16:53:02 -0400

    first commit taller-modas

---
+++ src/main/java/com/alvarto/taller_modas/TallerModasApplication.java
@@ -0,0 +1,13 @@
+package com.alvarto.taller_modas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerModasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerModasApplication.class, args);
+	}
+
+}
